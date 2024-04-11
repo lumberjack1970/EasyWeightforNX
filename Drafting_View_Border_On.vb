316602ca442b8af6a2895c9824ee4e774d0f1eb9
lumberjack1970 <164236127+lumberjack1970@users.noreply.github.com>
2024-04-11 11:39:07 +0100

    Added two useful Journals

---
+++ Drafting_View_Border_On.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports NXOpen
+
+Module NXJournal
+    Sub Main(ByVal args() As String)
+
+        Dim theSession As NXOpen.Session = NXOpen.Session.GetSession()
+        Dim workPart As NXOpen.Part = theSession.Parts.Work
+
+        ' Create a starting mark for undo 
+        Dim markId1 As NXOpen.Session.UndoMarkId
+        markId1 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Drafting View Border")
+
+        ' Create PreferencesBuilder to set preferences
+        Dim preferencesBuilder1 As NXOpen.Drafting.PreferencesBuilder
+        preferencesBuilder1 = workPart.SettingsManager.CreatePreferencesBuilder()
+
+        ' Setting the DisplayBorders to True
+        preferencesBuilder1.ViewWorkflow.DisplayBorders = True
+
+        ' Setting the ViewRepresentation to Exact
+        preferencesBuilder1.ViewStyle.ViewStyleGeneral.ViewRepresentation = NXOpen.Preferences.GeneralViewRepresentationOption.Exact
+
+        ' Commit changes and clean up
+        Dim nXObject1 As NXOpen.NXObject = Nothing
+        nXObject1 = preferencesBuilder1.Commit()
+
+        ' Set final undo mark
+        theSession.SetUndoMarkName(markId1, "Drafting Preferences")
+        preferencesBuilder1.Destroy()
+
+    End Sub
+End Module
---
+++ Layer 70_Hidden.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports NXOpen
+
+Module NXJournal
+Sub Main (ByVal args() As String) 
+
+Dim theSession As NXOpen.Session = NXOpen.Session.GetSession()
+Dim workPart As NXOpen.Part = theSession.Parts.Work
+
+Dim displayPart As NXOpen.Part = theSession.Parts.Display
+
+Dim stateArray1(70) As NXOpen.Layer.StateInfo
+
+stateArray1(70) = New NXOpen.Layer.StateInfo(70, NXOpen.Layer.State.Hidden)
+
+displayPart.Layers.SetObjectsVisibilityOnLayer(displayPart.ModelingViews.WorkView, stateArray1, True)
+
+theSession.CleanUpFacetedFacesAndEdges()
+
+End Sub
+End Module
